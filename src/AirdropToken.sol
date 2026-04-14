3f0646543fcc8f8cc7c87a67f2648fa4271485bf
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-14 18:28:37 +0200

    [add] - Token contract

--- src/AirdropToken.sol
+++ src/AirdropToken.sol
@@ -7,4 +7,17 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract AirdropToken is ERC20, Ownable {
     /* Constructor */
     constructor() ERC20("AirdropToken", "AT") Ownable(msg.sender) {}
+
+    /* Functions */
+    /**
+     * @notice Function that only the owner can call in order to mint
+     * all the tokens available in the contract.
+     * @param _account The account we want to mint the tokens to
+     * @param _value The amount we want to mint
+     * @dev Calls the internal _mint() function from the OpenZeppelin
+     * ERC20 contract
+     */
+    function mint(address _account, uint256 _value) external onlyOwner {
+        _mint(_account, _value);
+    }
 }
