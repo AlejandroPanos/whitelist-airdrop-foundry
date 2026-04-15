1f61b8b04edd02e88bae9aab204d84a56d123c16
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-15 17:54:50 +0200

    [add] - Deploy script file

---
+++ script/DeployAirdrop.s.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {WhitelistAirdrop, IERC20} from "src/WhitelistAirdrop.sol";
+import {AirdropToken} from "src/AirdropToken.sol";
+
+contract DeployAirdrop is Script {}
