315b9aaac7c51b1dd5cb67d062447bfd60be870a
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-15 21:35:29 +0200

    [add] - Testing users can claim

--- test/unit/WhitelistAirdropTest.t.sol
+++ test/unit/WhitelistAirdropTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console2} from "forge-std/Test.sol";
 import {AirdropToken} from "src/AirdropToken.sol";
 import {WhitelistAirdrop} from "src/WhitelistAirdrop.sol";
 import {DeployAirdrop} from "script/DeployAirdrop.s.sol";
@@ -16,7 +16,7 @@ contract WhitelistAirdropTest is Test {
     bytes32 private constant MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     uint256 private constant AMOUNT = 25 * 1e18;
 
-    bytes32 private constant PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 private constant PROOF_ONE = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 private constant PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] private proof;
 
@@ -33,5 +33,22 @@ contract WhitelistAirdropTest is Test {
 
         proof.push(PROOF_ONE);
         proof.push(PROOF_TWO);
+    }
+
+    /* Testing functions */
+    function testUsersCanClaim() public {
+        // Arrange
+        uint256 initialBalance = token.balanceOf(user);
+        bytes32 digest = airdrop.getMessage(user, AMOUNT);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+
+        // Act
+        vm.prank(gasPayer);
+        console2.log(proof.length);
+        airdrop.claim(user, AMOUNT, proof, v, r, s);
+        uint256 endingBalance = token.balanceOf(user);
+
+        // Assert
+        assertEq(endingBalance - initialBalance, AMOUNT);
     }
 }
