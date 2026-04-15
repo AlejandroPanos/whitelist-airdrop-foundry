3514af78dc96ed08a9b139da81b7b3d59437e9ba
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-15 21:19:19 +0200

    [add] - Unit testing basics

--- test/unit/WhitelistAirdropTest.t.sol
+++ test/unit/WhitelistAirdropTest.t.sol
@@ -2,5 +2,26 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {AirdropToken} from "src/AirdropToken.sol";
+import {WhitelistAirdrop} from "src/WhitelistAirdrop.sol";
+import {DeployAirdrop} from "script/DeployAirdrop.s.sol";
 
-contract WhitelistAirdropTest is Test {}
+contract WhitelistAirdropTest is Test {
+    /* Instantiate contracts */
+    AirdropToken token;
+    WhitelistAirdrop airdrop;
+    DeployAirdrop deployer;
+
+    /* State variables */
+    address user;
+    uint256 userPrivateKey;
+    address gasPayer;
+
+    /* Set up function */
+    function setUp() external {
+        deployer = new DeployAirdrop();
+        (airdrop, token) = deployer.run();
+        (user, userPrivateKey) = makeAddrAndKey("user");
+        gasPayer = makeAddr("gasPayer");
+    }
+}
