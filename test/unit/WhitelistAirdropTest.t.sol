773f314baeec6911d472a9c04e2d7550537eb8c0
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-15 21:21:15 +0200

    [add] - Rest of state variables

--- test/unit/WhitelistAirdropTest.t.sol
+++ test/unit/WhitelistAirdropTest.t.sol
@@ -13,6 +13,13 @@ contract WhitelistAirdropTest is Test {
     DeployAirdrop deployer;
 
     /* State variables */
+    bytes32 private constant MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private constant AMOUNT = 25 * 1e18;
+
+    bytes32 private constant PROOF_ONE = 0xd1445c931158119b00449ffcac3c947d028c0c359c34a6646d95962b3b55c6ad;
+    bytes32 private constant PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] private proof;
+
     address user;
     uint256 userPrivateKey;
     address gasPayer;
@@ -23,5 +30,8 @@ contract WhitelistAirdropTest is Test {
         (airdrop, token) = deployer.run();
         (user, userPrivateKey) = makeAddrAndKey("user");
         gasPayer = makeAddr("gasPayer");
+
+        proof.push(PROOF_ONE);
+        proof.push(PROOF_TWO);
     }
 }
