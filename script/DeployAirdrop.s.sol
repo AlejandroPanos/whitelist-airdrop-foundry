11ec9fe98f96b44e1b16cf8e47fe9f28793cc24f
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-15 18:00:18 +0200

    [add] - Deployment logic

--- script/DeployAirdrop.s.sol
+++ script/DeployAirdrop.s.sol
@@ -5,4 +5,23 @@ import {Script} from "forge-std/Script.sol";
 import {WhitelistAirdrop, IERC20} from "src/WhitelistAirdrop.sol";
 import {AirdropToken} from "src/AirdropToken.sol";
 
-contract DeployAirdrop is Script {}
+contract DeployAirdrop is Script {
+    /* Instantiate contracts */
+    WhitelistAirdrop airdrop;
+    AirdropToken token;
+
+    /* State variables */
+    bytes32 private constant MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private constant AMOUNT_TO_TRANSFER = 4 * (25 * 1e18);
+
+    /* Deployment function */
+    function run() external returns (WhitelistAirdrop, AirdropToken) {
+        vm.startBroadcast();
+        token = new AirdropToken();
+        airdrop = new WhitelistAirdrop(MERKLE_ROOT, IERC20(token));
+        token.mint(token.owner(), AMOUNT_TO_TRANSFER);
+        token.transfer(address(airdrop), AMOUNT_TO_TRANSFER);
+        vm.stopBroadcast();
+        return (airdrop, token);
+    }
+}
