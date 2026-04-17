f76ac5bfea4a3cd2253faa099f4625055174a6b8
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-17 18:44:11 +0200

    [add] - Handler logic

--- test/invariant/WhitelistAirdropHandler.t.sol
+++ test/invariant/WhitelistAirdropHandler.t.sol
@@ -2,5 +2,44 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
+import {WhitelistAirdrop} from "src/WhitelistAirdrop.sol";
 
-contract WhitelistAirdropHandler is Test {}
+contract WhitelistAirdropHandler is Test {
+    /* Instantiate contract */
+    WhitelistAirdrop airdrop;
+
+    /* State variable */
+    uint256 public s_totalClaimed;
+    mapping(address user => bool hasClaimed) public s_hasClaimed;
+
+    bytes32 private constant MERKLE_ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private constant AMOUNT = 25 * 1e18;
+
+    bytes32 private constant PROOF_ONE = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
+    bytes32 private constant PROOF_TWO = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
+    bytes32[] private proof;
+
+    address user;
+    uint256 userPrivateKey;
+
+    /* Constructor */
+    constructor(WhitelistAirdrop _airdrop) {
+        airdrop = _airdrop;
+        (user, userPrivateKey) = makeAddrAndKey("user");
+
+        proof.push(PROOF_ONE);
+        proof.push(PROOF_TWO);
+    }
+
+    /* Testing functions */
+    function claim() public {
+        if (airdrop.getClaimStatus(user)) return;
+
+        bytes32 digest = airdrop.getMessage(user, AMOUNT);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPrivateKey, digest);
+        airdrop.claim(user, AMOUNT, proof, v, r, s);
+
+        s_totalClaimed += AMOUNT;
+        s_hasClaimed[user] = true;
+    }
+}
