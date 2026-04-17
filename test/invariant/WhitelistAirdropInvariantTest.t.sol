677c9140744b6bac2f9bf27512bc1866f0c4f7bb
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-04-17 18:48:30 +0200

    [add] - Invariant file

---
+++ test/invariant/WhitelistAirdropInvariantTest.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {WhitelistAirdropHandler} from "./WhitelistAirdropHandler.t.sol";
+import {WhitelistAirdrop} from "src/WhitelistAirdrop.sol";
+
+contract WhitelistAirdropInvariantTest is Test {}
